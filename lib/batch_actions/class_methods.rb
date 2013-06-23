02dc92c77596dafafb619fabded87893235f406b
Victor Sokolov <gzigzigzeo@gmail.com>
2013-06-23 17:33:24 +0400

    Definition of single batch action

--- lib/batch_actions/class_methods.rb
+++ lib/batch_actions/class_methods.rb
@@ -5,5 +5,11 @@ module BatchActions
       @batch_actions.configure(self, &block) if block_given?
       @batch_actions
     end
+
+    def batch_action(action, options = {})
+      batch_actions do
+        batch_action action, options
+      end
+    end
   end
 end
